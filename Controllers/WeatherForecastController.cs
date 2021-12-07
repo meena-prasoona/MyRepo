269d80314a6ddf0a369b1e07089bdd5ff25246c5
meena-prasoona <chakka.meena@gmail.com>
2021-12-07 18:47:08 +0530

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Example.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","spring"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
